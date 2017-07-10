4b50670f8261aa71652d6ec01a36366612599582
fuzzybear3965 <johnrichardrinehart@gmail.com>
2017-07-10 11:24:49 -0400

    clean up main.go

--- main.go
+++ main.go
@@ -15,7 +15,6 @@ import (
 	"syscall"
 )
 
-// initialize the logger variable
 type loggerStruct struct {
 	Info     *log.Logger
 	Debug    *log.Logger
@@ -25,8 +24,6 @@ type loggerStruct struct {
 	Anything *log.Logger
 }
 
-var logger *loggerStruct
-
 var addrFlag = flag.String("address", "127.0.0.1:8080", "listening address and port \n\t(e.g. \"gotex --address 127.0.0.1:8080\" or \"gotex --address :8080\")\n\t")
 var logFlag = flag.String("logfile", "gotex.log", "log filename\n\t")
 var debugFlag = flag.Bool("debug", false, "debug?")
@@ -35,28 +32,34 @@ var debugFlag = flag.Bool("debug", false, "debug?")
 var WORKINGDIR, _ = filepath.Abs(filepath.Dir(os.Args[0]))
 
 func main() {
+	// parse the flags
 	flag.Parse()
-	logger, logfile := setUpLogger(*logFlag, log.Ldate|log.Ltime|log.Lshortfile|log.LUTC)
-	grabAssets()
+	// set up the logger
+	logger, logfile := initLogger(*logFlag, log.Ldate|log.Ltime|log.Lshortfile|log.LUTC)
+	// grab the assets
+	initAssets()
 	// Initialize the database
 	db := initDB("gotex.db")
 	defer db.Close()
 	migrate(db)
+	// Set up the router
 	r := vestigo.NewRouter()
-	// if it's the home page
+	// home page
 	r.Get("/", rootHandler)
-	// if it's a repo page
+	// repo page
 	r.Get("/:domain/:user/:repo", getHandler(db))
+	// repo page + branch
 	r.Get("/:domain/:user/:repo/:branch", getHandler(db))
+	// Push event
 	r.Post("/:domain/:user/:repo", postHandler(db))
 	// serve asset files
 	r.Get("/assets/*", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))).ServeHTTP)
-	// serve built files
+	// serve build files
 	r.Get("/builds/*", http.StripPrefix("/builds/", http.FileServer(http.Dir("builds"))).ServeHTTP)
-	// catch all
+	// catch-all
 	vestigo.CustomNotFoundHandlerFunc(defaultHandler)
-	// if it's the home page or some undefined route
 	logger.Info.Printf("----- gotex started: Listening on address %v in directory %v. -----\n", *addrFlag, WORKINGDIR)
+	// Proper clean-up (CTRL-C clean-up)
 	sigs := make(chan os.Signal, 1)
 	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
 	go func() {
@@ -65,35 +68,14 @@ func main() {
 		logfile.Close()
 		os.Exit(0)
 	}()
-
+	// serve at addrFlag
 	if err := http.ListenAndServe(*addrFlag, r); err != nil {
 		logger.Fatal.Println(err)
 	}
 }
 
-func grabAsset(path string) {
-	// taken from
-	// https://stackoverflow.com/questions/11692860/how-can-i-efficiently-download-a-large-file-using-go
-	file := fmt.Sprintf("assets/%v", path)
-	if _, err := os.Stat(file); os.IsNotExist(err) {
-		out, err := os.Create(file)
-		defer out.Close()
-		if err != nil {
-			logger.Error.Printf("Could not create asset file assets/%v.", path)
-		}
-		url := fmt.Sprintf("https://raw.githubusercontent.com/fuzzybear3965/gotex/master/%v", file)
-		resp, err := http.Get(url)
-		defer resp.Body.Close()
-		if err != nil {
-			logger.Error.Printf("Error downloading asset %v.", url)
-		}
-		if _, err := io.Copy(out, resp.Body); err != nil {
-			logger.Error.Printf("Could not write contents of file %v to %v.", url, path)
-		}
-	}
-}
-
-func grabAssets() {
+// Initialization functions
+func initAssets() {
 	// make the assets directory if it doesn't already exist
 	if err := os.MkdirAll("assets", os.ModePerm); err != nil {
 		logger.Error.Println(err)
@@ -113,7 +95,7 @@ func grabAssets() {
 	}
 }
 
-func setUpLogger(fn string, flags int) (*loggerStruct, *os.File) {
+func initLogger(fn string, flags int) (*loggerStruct, *os.File) {
 	// open up the logger file
 	file, err := os.OpenFile(fn, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 	if err != nil {
@@ -135,4 +117,27 @@ func setUpLogger(fn string, flags int) (*loggerStruct, *os.File) {
 		}
 	}
 	return logger, file
+}
+
+// Utility functions
+func grabAsset(path string) {
+	// taken from
+	// https://stackoverflow.com/questions/11692860/how-can-i-efficiently-download-a-large-file-using-go
+	file := fmt.Sprintf("assets/%v", path)
+	if _, err := os.Stat(file); os.IsNotExist(err) {
+		out, err := os.Create(file)
+		defer out.Close()
+		if err != nil {
+			logger.Error.Printf("Could not create asset file assets/%v.", path)
+		}
+		url := fmt.Sprintf("https://raw.githubusercontent.com/fuzzybear3965/gotex/master/%v", file)
+		resp, err := http.Get(url)
+		defer resp.Body.Close()
+		if err != nil {
+			logger.Error.Printf("Error downloading asset %v.", url)
+		}
+		if _, err := io.Copy(out, resp.Body); err != nil {
+			logger.Error.Printf("Could not write contents of file %v to %v.", url, path)
+		}
+	}
 }
